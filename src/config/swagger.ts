0ca2855ec8465a7a0c88dd60e1f0428d8a4a1144
Kellia <kelliarwagasana@gmail.com>
2026-05-05 19:35:43 +0200

    add production

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -15,6 +15,10 @@ const options: swaggerJsdoc.Options = {
         url: "http://localhost:3000",
         description: "Development server",
       },
+       {
+        url: "https://airbnb-api-1-fsuj.onrender.com/",
+        description: "Production server",
+      },
     ],
     components: {
       securitySchemes: {
